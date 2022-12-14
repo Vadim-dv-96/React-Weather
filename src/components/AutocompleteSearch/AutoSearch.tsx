f5c65c7cfa0329139e350e88deb13038dc5f46cb
Vadim <matuskinvadim198@gmail.com>
2022-12-14 18:06:45 +0200

    +

--- src/components/AutocompleteSearch/AutoSearch.tsx
+++ src/components/AutocompleteSearch/AutoSearch.tsx
@@ -1,5 +1,4 @@
 import { TextField } from '@mui/material';
-// import { RefObject, useState } from 'react';
 import { usePlacesWidget } from 'react-google-autocomplete';
 
 type AutoSearchPropsType = {
@@ -19,56 +18,13 @@ export const AutoSearch = ({ addCity }: AutoSearchPropsType) => {
     //   componentRestrictions: { country },
     // },
   });
-
-  //   const onKeyPressHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
-  //     if (e.key === 'Enter') {
-  //         addCityHandler();
-  //     }
-  //   };
   console.log(materialRef);
 
   return (
     <div>
       <div style={{ width: '300px' }}>
-        <TextField
-          onBlur={() => {
-            materialRef = { current: null };
-          }}
-          fullWidth
-          color="primary"
-          variant="outlined"
-          inputRef={materialRef}
-        />
+        <TextField fullWidth color="primary" variant="outlined" inputRef={materialRef} />
       </div>
-      {/* <div style={{ width: '250px' }}>
-        <span style={{ color: 'black' }}>Material UI</span>
-        <Input
-          //   onKeyPress={onKeyPressHandler}
-          fullWidth
-          color="primary"
-          inputComponent={({ inputRef, onFocus, onBlur, ...props }) => (
-            <Autocomplete
-              apiKey={process.env.REACT_APP_API_KEY_AUTOCOMPL}
-              {...props}
-              onPlaceSelected={(selected) => console.log(selected)}
-            />
-          )}
-        />
-      </div> */}
-
-      {/* <Autocomplete
-        apiKey={process.env.REACT_APP_API_KEY_AUTOCOMPL}
-        style={{ width: '90%' }}
-        onPlaceSelected={(place) => {
-          console.log(place);
-        }}
-        options={{
-          types: ['(regions)'],
-          componentRestrictions: { country: 'ua' },
-        }}
-        defaultValue="Amsterdam"
-      /> */}
-      {/* <TextField label="City" ref={ref} /> */}
     </div>
   );
 };
